8266033f332650294f8392d14d79272707fdc1a7
JorgeAFlores12 <48728819+JorgeAFlores12@users.noreply.github.com>
2019-05-22 11:49:44 -0500

    12

--- js/acciones.js
+++ js/acciones.js
@@ -21,6 +21,7 @@ function getPosition(){
 	var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 	
 	function onSuccess(position){
+		
 		alert('Latitude: '     + position.coords.latitude     + '\n' +
 		'Longitude: '     + position.coords.longitude     + '\n');
 	};
